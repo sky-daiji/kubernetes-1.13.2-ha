5a30cc4f65be790fce63f8ce01f9d25ba63f0de1
sky-daiji <45813918+sky-daiji@users.noreply.github.com>
2019-07-09 16:49:28 +0800

    Update k8s-kubeadm-init.sh

--- k8s-kubeadm-init.sh
+++ k8s-kubeadm-init.sh
@@ -8,7 +8,7 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
-yum install kubelet-1.14.3 kubeadm-1.14.3 kubectl-1.14.3
+yum install kubelet-1.14.3 kubeadm-1.14.3 kubectl-1.14.3 -y
 cat <<EOF > /etc/sysconfig/kubelet
 KUBELET_EXTRA_ARGS="--cgroup-driver=systemd --pod-infra-container-image=registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1"
 EOF
