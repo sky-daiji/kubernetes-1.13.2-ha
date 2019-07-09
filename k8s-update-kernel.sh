246e3c23af51dec953d3a9764d1fddec6c767f25
sky-daiji <45813918+sky-daiji@users.noreply.github.com>
2019-07-09 16:05:00 +0800

    Update k8s-update-kernel.sh

--- k8s-update-kernel.sh
+++ k8s-update-kernel.sh
@@ -24,7 +24,7 @@ wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 yum install wget git  jq psmisc -y
 yum update -y
 export Kernel_Vsersion=4.18.16-1
-wget http://software.mofangge.cc/linux/Kernel/kernel-ml-4.18.16-1.el7.elrepo.x86_64.rpm
+wget http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml{,-devel}-${Kernel_Vsersion}.el7.elrepo.x86_64.rpm
 yum localinstall -y kernel-ml*
 find /lib/modules -name '*nf_conntrack_ipv4*' -type f
 grub2-set-default  0 && grub2-mkconfig -o /etc/grub2.cfg
