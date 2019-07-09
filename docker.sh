7777e14a9092653a892642605baae004df014eea
sky-daiji <45813918+sky-daiji@users.noreply.github.com>
2019-07-09 16:13:45 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,11 +2,24 @@
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
-yum install docker-ce-17.06.2.ce -y
-sed -i "13i ExecStartPost=/usr/sbin/iptables -P FORWARD ACCEPT" /usr/lib/systemd/system/docker.service
+yum install docker-ce-18.09.2 -y
 systemctl daemon-reload && systemctl enable docker && systemctl start docker
-
-
+mkdir /etc/docker
+cat <<EOF > /etc/docker/daemon.json
+{
+    "exec-opts": ["native.cgroupdriver=systemd"],
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "100m",
+        "max-file": "10"
+    },
+    "bip": "169.254.123.1/24",
+    "oom-score-adjust": -1000,
+    "registry-mirrors": ["https://fz5yth0r.mirror.aliyuncs.com"],
+    "storage-driver": "overlay2",
+    "storage-opts":["overlay2.override_kernel_check=true"]
+}
+EOF
 
 
 
