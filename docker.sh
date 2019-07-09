10056d7699c0f184944758ceeccbaa1ca6bb9afb
sky-daiji <45813918+sky-daiji@users.noreply.github.com>
2019-07-09 16:14:07 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,7 +3,6 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
 yum install docker-ce-18.09.2 -y
-systemctl daemon-reload && systemctl enable docker && systemctl start docker
 mkdir /etc/docker
 cat <<EOF > /etc/docker/daemon.json
 {
@@ -20,6 +19,7 @@ cat <<EOF > /etc/docker/daemon.json
     "storage-opts":["overlay2.override_kernel_check=true"]
 }
 EOF
+systemctl daemon-reload && systemctl enable docker && systemctl start docker
 
 
 
